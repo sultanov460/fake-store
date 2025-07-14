e8524e55e94fe4e9c05117cb24c1eb64f2b1fb8c
Rafiq Sultanov <sultanovrafiq71@gmail.com>
2025-07-14 23:12:52 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Product from "./components/Product";
+import { PacmanLoader } from "react-spinners";
+export default function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("https://fakestoreapi.com/products");
+
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  if (loading) return <PacmanLoader color="#32cd32" />;
+  if (error) return <p>Ошибка: {error}</p>;
+  return (
+    <>
+      <Product products={products} />
+    </>
+  );
+}
